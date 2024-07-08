1a205e6dd20338e04913a5df28412bdc6ecac2cc
Rohit Sharma <erucix@gmail.com>
2024-07-08 11:59:43 +0530

    Added login dialog

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Colors } from "./util/Colors";
 import Drawer from "./components/Drawer";
 import Body from "./components/Body";
+import LoginDialog from "./components/LoginDialog";
+
+const loggedIn = false;
 
 const App = () => {
   const a = Colors();
   return (
     <>
+    {!loggedIn && <LoginDialog/>}
       <Drawer />
       <Body />
     </>
---
+++ src/components/LoginDialog.jsx
@@ -0,0 +1,47 @@
+import "../styles/LoginDialog.css";
+import { useState } from "react";
+
+const isLoggedIn = false;
+
+const LoginDialog = () => {
+  const [dialogVisible, setDialogVisible] = useState(!isLoggedIn);
+
+  const signInWithPassword = (visibility) => {
+    setDialogVisible(!visibility);
+  }
+
+  return (
+    <>
+      <div className={("background-cover ") + (dialogVisible ? "active" : "") }></div>
+      <div className={("dialog-container ") + (dialogVisible ? "active" : "")}>
+        <div className="login-dialog">
+          <h2>Login Here</h2>
+          <form>
+            <span className="input-container">
+              <span className="mdi mdi-at"></span>
+              <input type="email" placeholder="E-Mail" />
+            </span>
+            <span className="input-container">
+              <span className="mdi mdi-form-textbox-password"></span>
+              <input type="password" placeholder="Password" />
+            </span>
+            <button type="submit" onClick={()=>signInWithPassword(dialogVisible)}>LOGIN</button>
+            <a href="#">Forgot Username/Password?</a>
+            <span className="third-party-login">
+              <button>
+                <span className="mdi mdi-google"></span>
+                <span className="third-party-name">Google</span>
+              </button>
+              <button>
+                <span className="mdi mdi-github"></span>
+                <span className="third-party-name">Github</span>
+              </button>
+            </span>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LoginDialog;
