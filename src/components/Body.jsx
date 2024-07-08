4a84a6df03eaf913ac67fe896ceb4df169ddbe34
Rohit Sharma <erucix@gmail.com>
2024-07-08 10:08:39 +0530

    Popular Section

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,14 @@
 import "../styles/Body.css";
 import Navigation from "./Navigation";
 import Popular from "./Popular"
+import AllSongs from "./AllSongs.jsx";
 
 const Body = () => {
     return (
         <div className="main-body">
             <Navigation/>
             <Popular/>
+            <AllSongs />
         </div>
     );
 }
